29cc6786568d8a5fa19835cbe8da687d10fb449f
Muhammad Ahmad <26179@riphahfsd.edu.pk>
2026-05-19 16:29:26 +0500

    Add home page

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Hero from '@/components/sections/Hero';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      {/* Additional sections will be added here */}
+    </>
+  );
+}
